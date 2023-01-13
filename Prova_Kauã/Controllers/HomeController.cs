9279e3d538278ecde63113feb6ec2c5560825752
Kauã Alves <kaua.as@outlook.com>
2023-01-13 09:30:10 -0300

    Adicionar arquivos de projeto.

---
+++ Prova_Kauã/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prova_Kauã.Models;
+using System.Diagnostics;
+
+namespace Prova_Kauã.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("kaua")]
+        public IActionResult kaua()
+        {
+            return View();
+        }
+        public bool Login(UsuarioViewModel model)
+        {
+            try
+            {
+                if (model.EMAIL.ToLower() == "prova" && model.SENHA.ToLower() == "prova123")
+                    return true;
+
+                else
+                    return false;
+
+            }
+            catch (Exception ex)        
+            {
+                return false;
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Prova_Kauã/Models/UsuarioViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Prova_Kauã.Models
+{
+    public class UsuarioViewModel
+    {
+        public string EMAIL { get; set; }
+        public string SENHA { get; set; }
+    }
+}
